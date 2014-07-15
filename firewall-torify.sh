2f40d68f3acbc9b975fb1a4b0f78ed6502d142b5
Cédric Jeanneret <tengu@ethack.org>
2014-07-15 20:16:01 +0200

    using pure shell array, and added comment to iptables rules; now we know which App is forced through tor :)

--- firewall-torify.sh
+++ firewall-torify.sh
@@ -1,23 +1,25 @@
 . /data/local/iptables
 
-# redirect traffic for our friendly apps to polipo
+# redirect traffic for our friendly apps to TransPort
 for uid in $ALL_UIDS; do
-  $IPTABLES -t nat -A OUTPUT ! -o lo -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -m owner --uid-owner $uid -j REDIRECT --to-ports 9040
+  uid=$(echo $app | cut -d '=' -f 2)
+  name=$(echo $app | cut -d '=' -f 1)
+  $IPTABLES -t nat -A OUTPUT ! -o lo -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -m owner --uid-owner $uid -j REDIRECT --to-ports 9040 -m comment --comment "Force ${name} through TransPort"
 done
 # drop all unwanted output. because we can and we care :).
 $IPTABLES -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-# allow orbot output â €”€yes, that's what we really want :).
-$IPTABLES -A OUTPUT -m owner --uid-owner $ORBOT_UID -j ACCEPT
+# allow orbot output ? ???yes, that's what we really want :).
+$IPTABLES -A OUTPUT -m owner --uid-owner $ORBOT_UID -j ACCEPT -m comment --comment "Allow Orbot output"
 
 # Accept DNS requests to the Tor DNSPort.
-$IPTABLES -A OUTPUT -d 127.0.0.1/32 -p udp -m udp --dport 5400 -j ACCEPT
+$IPTABLES -A OUTPUT -d 127.0.0.1/32 -p udp -m udp --dport 5400 -j ACCEPT -m comment --comment "DNS Requests on Tor DNSPort"
 
 # allow local traffic to Polipo
-$IPTABLES -A OUTPUT -d 127.0.0.1/32 -p tcp -m tcp --dport 8118 --tcp-flags FIN,SYN,RST,ACK SYN -j ACCEPT
+$IPTABLES -A OUTPUT -d 127.0.0.1/32 -p tcp -m tcp --dport 8118 --tcp-flags FIN,SYN,RST,ACK SYN -j ACCEPT -m comment --comment "Local traffic to Polipo"
 
 # allow local traffic to TransPort
-$IPTABLES -A OUTPUT -d 127.0.0.1/32 -p tcp -m tcp --dport 9040 --tcp-flags FIN,SYN,RST,ACK SYN -j ACCEPT
+$IPTABLES -A OUTPUT -d 127.0.0.1/32 -p tcp -m tcp --dport 9040 --tcp-flags FIN,SYN,RST,ACK SYN -j ACCEPT -m comment --comment "Local traffic to TransPort"
 
-# Accept outgoing traffic to the local Tor SOCKSPorts.
-$IPTABLES -A OUTPUT -d 127.0.0.1/32 -p tcp -m tcp --dport 9050 --tcp-flags FIN,SYN,RST,ACK SYN -j ACCEPT
+# Accept outgoing traffic to the local Tor SOCKSPort.
+$IPTABLES -A OUTPUT -d 127.0.0.1/32 -p tcp -m tcp --dport 9050 --tcp-flags FIN,SYN,RST,ACK SYN -j ACCEPT -m comment --comment "Local traffic to SOCKSPort"
